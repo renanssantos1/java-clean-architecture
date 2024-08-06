d689de4c97bdd8e8c1b52fcf67a4d92795925250
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-08-06 19:39:23 -0300

    feat(domain): CPF classe com validacao [Domain Model].

---
+++ src/main/java/escola/CPF.java
@@ -0,0 +1,15 @@
+package escola;
+
+import java.util.Objects;
+
+public class CPF {
+    private String cpf;
+
+    public CPF(String numeroCpf) {
+        if (Objects.isNull(numeroCpf) || numeroCpf.matches("^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$\n")) {
+            throw new IllegalArgumentException("CPF invalido, corrija os campos");
+        }
+
+        this.cpf = numeroCpf;
+    }
+}
