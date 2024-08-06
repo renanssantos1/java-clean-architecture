69c5edf5c351b549e04fc1765d2feebea937149d
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-08-06 20:06:29 -0300

    feat(domain): validacao de null no telefone [Domain model].

--- src/main/java/escola/Telefone.java
+++ src/main/java/escola/Telefone.java
@@ -1,15 +1,17 @@
 package escola;
 
-import java.util.Objects;
-
 public class Telefone {
     private String ddd;
     private String numero;
 
     public Telefone(String ddd, String numero) {
 
-        if (ddd.matches("^\\d{2}$\n") || numero.matches("^\\d{8,9}$\n")){
+        if (!ddd.matches("^\\d{2}$\n") || !numero.matches("^\\d{8,9}$\n")){
             throw new IllegalArgumentException("Formato do telefone invalido");
+        }
+
+        if (ddd == null || numero == null){
+            throw new IllegalArgumentException("DDD e numero obrigatorios");
         }
 
         this.ddd = ddd;
