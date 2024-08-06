809d15573a2662bc386001bcb33897f8ce34beff
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-08-06 20:20:51 -0300

    feat(domain):ajuste validacao e getter do telefone [Domain model].

--- src/main/java/escola/Telefone.java
+++ src/main/java/escola/Telefone.java
@@ -6,15 +6,23 @@ public class Telefone {
 
     public Telefone(String ddd, String numero) {
 
-        if (!ddd.matches("^\\d{2}$\n") || !numero.matches("^\\d{8,9}$\n")){
-            throw new IllegalArgumentException("Formato do telefone invalido");
-        }
-
         if (ddd == null || numero == null){
             throw new IllegalArgumentException("DDD e numero obrigatorios");
         }
 
+        if (!ddd.matches("^\\d{2}$\n") || !numero.matches("^\\d{8,9}$\n")){
+            throw new IllegalArgumentException("Formato do telefone invalido");
+        }
+
         this.ddd = ddd;
         this.numero = numero;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public String getNumero() {
+        return numero;
     }
 }
