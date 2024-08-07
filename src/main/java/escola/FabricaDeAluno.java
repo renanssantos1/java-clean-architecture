5f1fd6474c2e31b92e3ceb33991b51e95b71017c
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-08-07 20:36:27 -0300

    fix(domain) instancia de criacao de aluno uma vez [Domain model].

--- src/main/java/escola/FabricaDeAluno.java
+++ src/main/java/escola/FabricaDeAluno.java
@@ -4,18 +4,48 @@ public class FabricaDeAluno {
 
     private Aluno aluno;
 
-    public FabricaDeAluno comCPFNomeEmail(String nome, String cpf, String email) {
-        this.aluno = new Aluno(new CPF(cpf), nome, new Email(email));
-        return this;
+    private FabricaDeAluno() {
     }
 
-    public FabricaDeAluno comTelefone(String ddd, String numero) {
-        this.aluno.adicionarTelefone(ddd, numero);
-        return this;
+    public static class FabricaComCPFNomeEmail {
+        private Aluno aluno;
+
+        private FabricaComCPFNomeEmail(Aluno aluno) {
+            this.aluno = aluno;
+        }
+
+        public FabricaComTelefone comTelefone(String ddd, String numero) {
+            this.aluno.adicionarTelefone(ddd, numero);
+            return new FabricaComTelefone(aluno);
+        }
+
+        public Aluno build() {
+            return this.aluno;
+        }
     }
 
-    public Aluno criarAluno(){
-        return this.aluno;
+    public static class FabricaComTelefone {
+
+        private Aluno aluno;
+
+        private FabricaComTelefone(Aluno aluno) {
+            this.aluno = aluno;
+        }
+
+        public Aluno build() {
+            return this.aluno;
+        }
     }
 
+    @Override
+    public String toString() {
+        return "FabricaDeAluno{" +
+                "aluno=" + aluno +
+                '}';
+    }
+
+    public static FabricaComCPFNomeEmail comCPFNomeEmail(String nome, String cpf, String email) {
+        Aluno aluno = new Aluno(new CPF(cpf), nome, new Email(email));
+        return new FabricaComCPFNomeEmail(aluno);
+    }
 }
